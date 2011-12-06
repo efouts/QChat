6ec0fd9cd57d8a4614d03bb8b8d914b1f183ba02
efouts <none@none>
2011-12-06 14:27:07 -0500

    merged static content

--- static/scripts/client.js
+++ static/scripts/client.js
@@ -1,48 +1,35 @@
-var QChatClient = function QChatClient(messageField, messageListField) {
-        var sinceTimestamp = null;
+function chatClient() {
+    this.MessagesPanel = undefined;
+    this.MessageTextBox = undefined;
+    this.PostMessageButton = undefined;
+    this.LastMessageReceivedDate = undefined;
 
-        var send = function send(message) {
-                $.post('/send', { content: message, nickname: 'elliott' });           
-        };
-
-        this.initialize = function initialize() {
-                loadMessages();
-                messageField.keypress(messageFieldOnKeypress);          
-        };
-
-        var messageFieldOnKeypress = function messageFieldOnKeypress(e) {
-                if (e.which != 13)
-                        return;
+    this.initialize = function(messagesPanelId, textboxId, buttonId) {
+        this.MessagesPanel = $('#' + messagesPanelId);
+        this.MessageTextBox = $('#' + textboxId);
+        this.PostMessageButton = $('#' + buttonId);
         
-                var message = messageField.val().replace('\n', '');
+        this.PostMessageButton.click({chat:this}, this.PostMessageButtonClick);
+    }
+    
+    this.PostMessageButtonClick = function(e) {
+        var chat = e.data.chat;
+        chat.PostMessage();
+        chat.MessageTextBox.val('');
+    }
+    
+    this.PostMessage = function() {
+        this.MessagesPanel.append('<div>' + this.MessageTextBox.val() + '</div>');
+    }
+    
+    this.MessageReceived = function() {
+        // Todo add the message to the display div
+    }
+    
+    this.UserConnected = function() {
+        // Todo add a message to the display to show the user connected    
+        // Todo add a message to the connected users panel.
+    }
+}
 
-                if (message != '')
-                        send(message);
-
-                messageField.val('');
-        };
-
-        var loadMessages = function loadMessage() {
-
-		var callback = function(messages) {
-			appendMessages(messages);
-			loadMessages();
-		};
-
-		if (sinceTimestamp == null)
-	                $.getJSON('/messages', {}, callback);
-		else 
-			$.getJSON('/messages', { since: sinceTimestamp }, callback);
-        };
-
-	var appendMessages = function displayMessages(messages) {
-		$.each(messages, function(index, message) {
-			appendMessage(message);
-			sinceTimestamp = message.timestamp;
-		});
-	};
-
-        var appendMessage = function appendMessage(message) { 
-                messageListField.append('<li>' + message.nickname + ': ' + message.content + '</li>');
-        };      
-};
+var qchat = new chatClient();
