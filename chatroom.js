6f41c89af47753131adc3daa162e2451a415ac70
efouts <none@none>
2011-12-08 21:03:12 -0500

    refactored server-side. sending data to long running connections is now event based. added message type to message data.

--- chatroom.js
+++ chatroom.js
@@ -1,48 +1,48 @@
+var events = require('events');
+var util = require('util');
+var message = require('./message.js');
+
 var chatroom = function chatroom() {
+    events.EventEmitter.call(this);
     var self = this;
 	var messages = [];
 	
-    this.enter = function enter(alias) {
-        var message = alias + ' has entered.';
-        sendServerMessage(message);
+    this.join = function join(alias) {
+        var content = alias + ' has joined.';
+        self.sendMessage('Server', content, new Date(), message.types.join);
     };
 
-    this.exit = function exit(alias) {
-        var message = alias + ' has exited.';
-        sendServerMessage(message);
+    this.leave = function leave(alias) {
+        var content = alias + ' has left.';
+        self.sendMessage('Server', content, new Date(), message.types.leave);
     };
 
     this.changeAlias = function changeAlias(previousAlias, newAlias) {
-        var message = previousAlias + ' is now ' + newAlias;
-        sendServerMessage(message);
+        var content = previousAlias + ' is now ' + newAlias + '.';
+        self.sendMessage('Server', content, new Date(), message.types.alias);
     };
 
-	this.sendMessage = function sendMessage(alias, message, timestamp) {
-        var message = createMessage(alias, message, timestamp);
-		messages.push(message);
+	this.sendMessage = function sendMessage(alias, content, timestamp, type) {
+        var _message = message.create(alias, content, timestamp, type);
+		messages.push(_message);
+
+        self.emit('message');
 	};	
 
-	this.getMessages = function getMessages(since) {
-		if (since == undefined)			
-			return messages;
-		
-		var messagesSince = [];
+    this.findAllMessages = function findAllMessages() {
+        return messages;
+    };
 
-		for(i = 0; i < messages.length; i++)
-			if (messages[i].timestamp > since)
-				messagesSince.push(messages[i]);
+	this.findMessages = function findMessages(since) {
+		var messagesSince = [];
+        var messageIndex = messages.length - 1;
+
+        while (messages[messageIndex].timestamp > since && messageIndex >= 0)
+            messagesSince.unshift(messages[messageIndex--]);        
 
 		return messagesSince;
 	};
-
-    var sendServerMessage = function sendServerMessage(message) {
-        var timestamp = new Date();
-        self.sendMessage('Server', message, timestamp);
-    };
-
-	var createMessage = function createMessage(alias, content, timestamp) {
-		return { alias: alias, content: content, timestamp: timestamp };
-	};
 };
 
+util.inherits(chatroom, events.EventEmitter);
 module.exports = chatroom;
