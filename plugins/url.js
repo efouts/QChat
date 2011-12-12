e498e7781ae72d96b9655185c1a254747b249f76
efouts <none@none>
2011-12-11 22:14:39 -0500

    Modified url regex

--- plugins/url.js
+++ plugins/url.js
@@ -3,14 +3,14 @@ var urlPlugin = function urlPlugin() {
         if (action.type !== 'message')
             return action;
 
-        var urlRegex = /((?:http|https):\/\/[a-z0-9\/\?=_#&%~-]+(\.[a-z0-9\/\?=_#&%~-]+)+)|(www(\.[a-z0-9\/\?=_#&%~-]+){2,})/gi;
+        var urlRegex = /(((http|ftp|https):\/\/)|www\.)[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#!]*[\w\-\@?^=%&/~\+#])?/gi;
         action.content = action.content.replace(urlRegex, replaceUrl);
 
         return action;
     };
 
-    var replaceUrl = function replaceUrl(match, backReference1) {
-        return '<a href="' + backReference1 + '" target="_blank">' + backReference1 + '</a>';
+    var replaceUrl = function replaceUrl(match) {
+        return '<a href="' + match + '" target="_blank">' + match + '</a>';
     };
 };
 
