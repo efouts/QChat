093dfe601b423f992b0bc77c0c87570c4b090481
efouts <none@none>
2011-12-13 18:00:27 -0500

    Removed utilities included with connect

--- utilities.js
+++ utilities.js
@@ -1,12 +1,3 @@
-module.exports.htmlEncode = function htmlEncode(string) {
-    return String(string)
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-}; 
-
 module.exports.jsonResponse = function jsonResponse(data, response) {
     var jsonData = JSON.stringify(data);
     response.writeHead(200, 'application/json');
