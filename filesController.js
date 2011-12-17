0016d10dfd5c0bae8fe4ba023cc3acf7c12bc62e
efouts <none@none>
2011-12-16 23:45:09 -0500

    Output upload percentage to console
    --HG--
    branch : v3-dev

--- filesController.js
+++ filesController.js
@@ -1,14 +1,29 @@
+var multipart = require('multipart');
 var utils = require('./utilities.js');
 
 var filesController = function filesController(activityLog) {
-    this.upload = function (request, response) {
-        var entry = { };
-        activityLog.addEntry(entry);
-    };
+  this.upload = function (request, response) {
+    request.setBodyEncoding('binary');
+    var stream = new multipart.Stream(request);
+    
+    stream.addListener('part', function(part) {
+      part.addListener('body', function(chunk) {
+        var progress = (stream.bytesReceived / stream.bytesTotal * 100).toFixed(20);
+        console.log(progress '% complete');
+      });
+    });
 
-    this.download = function(request, response) {
+    stream.addListener('complete', function() {
+      utils.emptyResponse(response);
 
-    };
+      var entry = { type: 'file' };
+      activityLog.addEntry(entry);
+    });
+  };
+
+  this.download = function(request, response) {
+
+  };
 };
 
 module.exports = filesController;
