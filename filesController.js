a5b69099862aba04a395234b2df77aa54e61993f
efouts <none@none>
2011-12-17 00:36:08 -0500

    Added formidable module
    --HG--
    branch : v3-dev

--- filesController.js
+++ filesController.js
@@ -1,23 +1,13 @@
-var multipart = require('multipart');
+var formidable = require('formidable');
 var utils = require('./utilities.js');
 
 var filesController = function filesController(activityLog) {
   this.upload = function (request, response) {
-    request.setBodyEncoding('binary');
-    var stream = new multipart.Stream(request);
-    
-    stream.addListener('part', function(part) {
-      part.addListener('body', function(chunk) {
-        var progress = (stream.bytesReceived / stream.bytesTotal * 100).toFixed(20);
-        console.log(progress '% complete');
-      });
-    });
-
-    stream.addListener('complete', function() {
+    var form = new formidable.IncomingForm();
+    form.parse(request, function (err, fields, files) {
       utils.emptyResponse(response);
-
-      var entry = { type: 'file' };
-      activityLog.addEntry(entry);
+      console.log('fields: ' + fields);
+      console.log('files: ' + files);
     });
   };
 
