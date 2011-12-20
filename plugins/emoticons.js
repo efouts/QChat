3f37b8d575a1c4ec8a3eeaf42803371fb6d480ca
funktionman <none@none>
2011-12-20 13:02:53 -0500

    Added initial draft of emoticon plugin.
    --HG--
    branch : v3-dev

---
+++ plugins/emoticons.js
@@ -0,0 +1,33 @@
+var emoticonsPlugin = function emoticonsPlugin() {
+    this.apply = function apply(action) {
+        if (action.type !== 'message')
+            return action;
+
+        
+        action.content = action.content.replace(regex, replaceWithEmoticon);
+
+        return action;
+    };
+
+    var buildRegex = function buildRegex() {
+        var keys = [];
+
+        for (var key in emoticons)
+            keys.push(escapeKey(key));
+
+        return new RegExp('(' + keys.join('|') + ')');
+    };
+
+    var escapeKey = function escapeKey(key) {
+        return key.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+    }
+
+    var replaceWithEmoticon = function replaceWithEmoticon(match) {
+        return '<img src="images/emoticons/' + emoticons[match] + '" />';
+    };
+
+    var emoticons = { ':)': 'happy.gif' };
+    var regex = buildRegex();
+};
+
+module.exports = emoticonsPlugin;
