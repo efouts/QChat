ebf035055d705c7bdeaf32f339c2ea6e8948e841
funktionman <none@none>
2011-12-20 13:22:33 -0500

    Completed the dictionary of emoticons to images.
    --HG--
    branch : v3-dev

--- plugins/emoticons.js
+++ plugins/emoticons.js
@@ -1,10 +1,7 @@
 var emoticonsPlugin = function emoticonsPlugin() {
     this.apply = function apply(action) {
-        if (action.type !== 'message')
-            return action;
-
-        
-        action.content = action.content.replace(regex, replaceWithEmoticon);
+        if (action.type === 'message')
+            action.content = action.content.replace(regex, replaceWithEmoticon);
 
         return action;
     };
@@ -26,7 +23,72 @@ var emoticonsPlugin = function emoticonsPlugin() {
         return '<img src="images/emoticons/' + emoticons[match] + '" />';
     };
 
-    var emoticons = { ':)': 'happy.gif' };
+    var emoticons = {
+        ':)': 'happy.gif',
+        ':(': 'sad.gif',
+        ';)': 'winking.gif',
+        ':D': 'big_grin.gif',
+        ';;)': 'eyelashes.gif',
+        '>:D<': 'big_hug.gif',
+        ':-/': 'confused.gif',
+        ':x': 'love_struck.gif',
+        ':">': 'blushing.gif',
+        ':P': 'tongue.gif',
+        ':-*': 'kiss.gif',
+        '=((': 'broken_heart.gif',
+        ':-O': 'surprise.gif',
+        'X(': 'angry.gif',
+        ':>': 'smug.gif',
+        'B-)': 'cool.gif',
+        ':-S': 'worried.gif',
+        '#:-S': 'whew.gif',
+        '>:)': 'devil.gif',
+        ':((': 'crying.gif',
+        ':))': 'laughing.gif',
+
+        ':|': 'straight_face.gif',
+        '/:)': 'raised_eyebrows.gif',
+        '=))': 'rotfl.gif',
+        'O:-)': 'angel.gif',
+        ':-B': 'nerd.gif',
+        '=;': 'talk_to_the_hand.gif',
+        ':-c': 'call_me.gif',
+        ':)]': 'on_the_phone.gif',
+        '~X(': 'at_wits_end.gif',
+        ':-h': 'wave.gif',
+        ':-t': 'time_out.gif',
+        ':8->': 'day_dreaming.gif',
+        'I-)': 'sleepy.gif',
+        '8-|': 'rolling_eyes.gif',
+        'L-)': 'loser.gif',
+        ':-&': 'sick.gif',
+        ':-$': 'dont_tell.gif',
+        '[-(': 'no_talking.gif',
+        ':O)': 'clown.gif',
+        '8-}': 'silly.gif',
+        '<:-P': 'party.gif',
+
+        '(:|': 'yawn.gif',
+        '=P~': 'drooling.gif',
+        ':-?': 'thinking.gif',
+        '#-o': 'doh.gif',
+        '=D>': 'applause.gif',
+        ':-SS': 'nail_biting.gif',
+        '@-)': 'hyptonized.gif',
+        ':^o': 'liar.gif',
+        ':-w': 'waiting.gif',
+        ':-<': 'sigh.gif',
+        '>:P': 'phbbbbt.gif',
+        '<):)': 'cowboy.gif',
+        'X_X': 'dont_wanna_see.gif',
+        ':!!': 'hurry_up.gif',
+        '\m/': 'rock_on.gif',
+        ':-q': 'thumbs_down.gif',
+        ':-bd': 'thumbs_up.gif',
+        '^#(^': 'it_wasnt_me.gif',
+        ':ar!': 'pirate.gif'
+    };
+
     var regex = buildRegex();
 };
 
