f254549d227f7c701814105fb0f6d464899695ea
funktionman <none@none>
2011-12-30 16:43:34 -0500

    Updated the OnTime plugin to include support for tasks.  Updated the style of links inside of your messages to be black.
    --HG--
    branch : v3-dev

--- plugins/ontime.js
+++ plugins/ontime.js
@@ -2,22 +2,33 @@ var ontime = function ontime() {
     this.apply = function apply(action) {
         if (action.type !== 'message')
             return action;
-    
-        var defectRegex = /%d([0-9]+)/gi;        
+
+        var defectRegex = /%d([0-9]+)/gi;
         var featureRegex = /%f([0-9]+)/gi;
-        
+        var taskRegex = /%t([0-9]+)/gi;
+        var incidentRegex = /%i([0-9]+)/gi;
+
         action.content = action.content.replace(defectRegex, replaceDefect)
-                                       .replace(featureRegex, replaceFeature);        
+                                       .replace(featureRegex, replaceFeature)
+                                       .replace(taskRegex, replaceTask);
 
         return action;
     };
 
     var replaceDefect = function replaceDefect(match, backReference1) {
-        return '<a href="ontime:0_' + backReference1 + '" target="_blank">OnTime Defect #' + backReference1 + '</a>';
+        return generateLink(0, backReference1, "Defect");
     };
 
     var replaceFeature = function replaceFeature(match, backReference1) {
-        return '<a href="ontime:1_' + backReference1 + '" target="_blank">OnTime Feature #' + backReference1 + '</a>';
+        return generateLink(1, backReference1, "Feature");
+    };
+
+    var replaceTask = function replaceTask(match, backReference1) {
+        return generateLink(2, backReference1, "Task");
+    };
+
+    var generateLink = function generateLink(onTimePrefixNumber, onTimeItemNumber, onTimeItemName) {
+        return '<a href="ontime:' + onTimePrefixNumber + '_' + onTimeItemNumber + '" target="_blank">OnTime ' + onTimeItemName + ' #' + onTimeItemNumber + '</a>';
     };
 };
 
