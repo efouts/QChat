00308e918739fe41002ea3d7218aa990ce3f1e9c
funktionman <none@none>
2013-08-16 15:39:36 -0400

    Accepting data from posts to the /plugins/jenkins
    --HG--
    branch : v3-dev

--- plugins/jenkins.js
+++ plugins/jenkins.js
@@ -2,16 +2,23 @@ var utils = require('../utilities.js');
 
 var jenkins = function jenkins() {
     this.handle = function handle(request, response, chatroom) {
-        var jsonContent;
+        console.log(request.body);
+        var jsonContent = request.body;
 
-        // HACK!! The JSON in the body is wrapped in JSON. Look at the body parser????
-        for (first in request.body) {
-            jsonContent = JSON.parse(first);
-            break;
-        }
+        //// HACK!! The JSON in the body is wrapped in JSON. Look at the body parser????
+        //for (first in request.body) {
+        //    jsonContent = JSON.parse(first);
+        //    break;
+        //}
         
-        var content = "Jenkins Notification: Job " + jsonContent.name + " updated with phase " + jsonContent.build.phase;
-        chatroom.sendMessage('jenkins', content);
+        var content;
+
+        //if (jsonContent.message)
+        //    content = jsonContent.message;
+        //else
+        //    content = "Jenkins Notification: Job " + jsonContent.name + " updated with phase " + jsonContent.build.phase;
+
+        chatroom.sendMessage('jenkins', jsonContent);
     };
 };
 
